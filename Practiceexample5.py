7877277924a188534a1ecdb4465428a863e3ae8d
Shreyash Bhise <108046802+Shreyashbhise@users.noreply.github.com>
2023-11-04 23:41:20 +0530

    Add files via upload

---
+++ Practiceexample5.py
@@ -0,0 +1,31 @@
+import boto3
+import os
+import shutil
+
+# Replace these with your AWS access key and secret key
+aws_access_key = 'YOUR_ACCESS_KEY'
+aws_secret_key = 'YOUR_SECRET_KEY'
+aws_region = 'us-east-1'  # Update with your AWS region
+s3_bucket_name = 'your-s3-bucket-name'
+
+# Source directory to be backed up
+source_directory = '/path/to/source_directory'
+
+# Destination directory for the backup
+backup_directory = '/path/to/backup_directory'
+
+# Backup: Copy the contents of the source directory to the backup directory
+shutil.copytree(source_directory, backup_directory)
+print("Backup completed successfully. Files copied to: {}".format(backup_directory))
+
+# Restore: Copy the contents from the backup directory to the source directory
+shutil.rmtree(source_directory)
+shutil.copytree(backup_directory, source_directory)
+print("Restored backup to {}".format(source_directory))
+
+# Initialize the Boto3 S3 client
+s3 = boto3.client('s3', region_name=aws_region, aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
+
+# Upload the backup to an S3 bucket
+s3.upload_file(backup_directory, s3_bucket_name, 'backup.zip')
+print("Backup uploaded to S3 bucket: {}".format(s3_bucket_name))
---
+++ practiceexampl4.py
@@ -0,0 +1,23 @@
+import boto3
+
+# Replace these with your AWS access key and secret key
+aws_access_key = 'YOUR_ACCESS_KEY'
+aws_secret_key = 'YOUR_SECRET_KEY'
+aws_region = 'us-east-1'  # Update with your AWS region
+
+# Initialize the Boto3 EC2 client
+ec2 = boto3.client('ec2', region_name=aws_region, aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
+
+# Define the tag criteria for instance termination
+tag_filter = [{'Name': 'tag:Environment', 'Values': ['Development']},
+              {'Name': 'tag:AutoTerminate', 'Values': ['True']}]
+
+# List instances based on the tag criteria
+response = ec2.describe_instances(Filters=tag_filter)
+
+# Terminate instances that match the criteria
+for reservation in response['Reservations']:
+    for instance in reservation['Instances']:
+        instance_id = instance['InstanceId']
+        ec2.terminate_instances(InstanceIds=[instance_id])
+        print("Terminated instance {}".format(instance_id))
